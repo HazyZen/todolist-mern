2311662a39438064e02f1d788fa7fc288cce5390
HazyZen <vio.waqas@gmail.com>
2025-02-26 14:27:18 +0800

    cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const TodoModel = require("./Models/Todos");
 
 const app = express();
@@ -10,26 +11,14 @@ const mongoUrl = process.env.MONGO_ATLAS_URL;
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://todo-mern-frontend-wine.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
+const corsOptions = {
+  origin: "https://todo-mern-frontend-wine.vercel.app",
+  credentials: true,
+  optionSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 mongoose
   .connect(`${mongoUrl}`, { useNewUrlParser: true, useUnifiedTopology: true })
